f07f7d81396a97de32591d2e65fd12ffa8ab80c4
vurtnewk <vurtnewk@163.com>
2025-01-07 14:17:18 +0800

    48、商品服务-API-三级分类-查询-树形展示三级分类数据

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data () {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'childrenList',
+        label: 'name'
+      }
+    }
+  },
+  methods: {
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({ data }) => {
+        console.log('成功获取到菜单数据...', data.data)
+        this.menus = data.data
+      })
+    },
+    handleNodeClick (data) {
+      console.log(data)
+    }
+  },
+  created () {
+    this.getMenus()
+  }
+}
+</script>
+
+<template>
+  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<style scoped lang="scss">
+
+</style>
